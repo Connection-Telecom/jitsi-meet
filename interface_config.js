0a447713f5ede42394768534a83fac5034134346
Steve Davies <steve@connection-telecom.com>
2020-04-11 21:27:30 +0200

    interface-config as per conf.telviva.com

--- interface_config.js
+++ interface_config.js
@@ -9,29 +9,29 @@ var interfaceConfig = {
      * Whether or not the blurred video background for large video should be
      * displayed on browsers that can support it.
      */
-    DISABLE_VIDEO_BACKGROUND: false,
+    DISABLE_VIDEO_BACKGROUND: true,
 
     INITIAL_TOOLBAR_TIMEOUT: 20000,
     TOOLBAR_TIMEOUT: 4000,
     TOOLBAR_ALWAYS_VISIBLE: false,
     DEFAULT_REMOTE_DISPLAY_NAME: 'Fellow Telvivist',
-    DEFAULT_LOCAL_DISPLAY_NAME: 'me',
+    DEFAULT_LOCAL_DISPLAY_NAME: 'Me',
     SHOW_JITSI_WATERMARK: true,
-    JITSI_WATERMARK_LINK: 'https://jitsi.org',
+    JITSI_WATERMARK_LINK: 'https://telviva.co.za/',
 
     // if watermark is disabled by default, it can be shown only for guests
-    SHOW_WATERMARK_FOR_GUESTS: true,
-    SHOW_BRAND_WATERMARK: false,
-    BRAND_WATERMARK_LINK: '',
+    SHOW_WATERMARK_FOR_GUESTS: false,
+    SHOW_BRAND_WATERMARK: true,
+    BRAND_WATERMARK_LINK: 'https://telviva.co.za/',
     SHOW_POWERED_BY: false,
     SHOW_DEEP_LINKING_IMAGE: false,
     GENERATE_ROOMNAMES_ON_WELCOME_PAGE: true,
     DISPLAY_WELCOME_PAGE_CONTENT: true,
     DISPLAY_WELCOME_PAGE_TOOLBAR_ADDITIONAL_CONTENT: false,
-    APP_NAME: 'Telviva One',
+    APP_NAME: 'Telviva One Conferencing',
     NATIVE_APP_NAME: 'Telviva One',
-    PROVIDER_NAME: 'Jitsi',
-    LANG_DETECTION: true, // Allow i18n to detect the system language
+    PROVIDER_NAME: 'Telviva',
+    LANG_DETECTION: false, // Allow i18n to detect the system language
     INVITATION_POWERED_BY: true,
 
     /**
@@ -60,7 +60,7 @@ var interfaceConfig = {
     // screen, 'height' would fit the original video height to the height of the
     // screen, 'width' would fit the original video width to the width of the
     // screen respecting ratio.
-    VIDEO_LAYOUT_FIT: 'both',
+    VIDEO_LAYOUT_FIT: 'height',
 
     /**
      * Whether to only show the filmstrip (and hide the toolbar).
@@ -108,7 +108,7 @@ var interfaceConfig = {
     LIVE_STREAMING_HELP_LINK: 'https://jitsi.org/live',
 
     /**
-     * Whether the mobile app Telviva One is to be promoted to participants
+     * Whether the mobile app Jitsi Meet is to be promoted to participants
      * attempting to join a conference in a mobile Web browser. If
      * {@code undefined}, defaults to {@code true}.
      *
@@ -128,7 +128,7 @@ var interfaceConfig = {
      * If indicated some of the error dialogs may point to the support URL for
      * help.
      */
-    SUPPORT_URL: 'https://community.jitsi.org/',
+    SUPPORT_URL: 'https://telviva.co.za/telviva-conf/user-guides/',
 
     /**
      * Whether the connection indicator icon should hide itself based on
@@ -139,7 +139,7 @@ var interfaceConfig = {
      *
      * @type {boolean}
      */
-    CONNECTION_INDICATOR_AUTO_HIDE_ENABLED: true,
+    CONNECTION_INDICATOR_AUTO_HIDE_ENABLED: false,
 
     /**
      * How long the connection indicator should remain displayed before hiding.
@@ -206,12 +206,12 @@ var interfaceConfig = {
     * If this is set to false, the banner will not be rendered at all. If set to true, the check for extension(s)
     * being already installed is done before rendering.
     */
-    SHOW_CHROME_EXTENSION_BANNER: false
+    SHOW_CHROME_EXTENSION_BANNER: true
 
     /**
      * When enabled, the kick participant button will not be presented for users without a JWT
      */
-    // HIDE_KICK_BUTTON_FOR_GUESTS: false
+    // HIDE_KICK_BUTTON_FOR_GUESTS: false,
 
     /**
      * How many columns the tile view can expand to. The respected range is
